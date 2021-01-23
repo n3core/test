e76aa9ff3fef35b51ae5490770c31beacdfe2a85
n3core <77888238+n3core@users.noreply.github.com>
2021-01-23 18:03:11 +0300

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+str = 'Hello world!'
+print (str)
+
+name =  input('Введите ваше имя: ')
+age =  input('Введите ваш возраст: ')
+
+print("Ваше имя " +  name + " " +  "Ваш возраст " +  age)
+print("По моим расчетам вы...")
+print("Педик")
